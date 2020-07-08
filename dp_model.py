fd47b24614c40b153ccde582996999a5037b544a
yihengy <yihengy@uchicago.edu>
2020-07-08 21:40:16 +0800

    fix

--- dp_model.py
+++ dp_model.py
@@ -5,6 +5,8 @@ https://github.com/SaoYan/DnCNN-PyTorch/blob/master/models.py
 
 import numpy as np
 import torch.nn as nn
+import torch.nn.functional as f
+from torch.autograd import Variable
 
 #Direct Prediction COnvolutional Network
 class dp_cnn(nn.Module):
@@ -15,7 +17,7 @@ class dp_cnn(nn.Module):
         #ker_size, pad_size, features, and bias_val should be determined by experiment
         ker_size = 21 #TBD
         pad_size = 1 #to avoid edge-data being underrepresented
-        features = 32 #tbd
+        features = 64 #tbd
         '''
         **********Network structure**********
         In each layer l, the network applies a linear convolution
@@ -25,20 +27,19 @@ class dp_cnn(nn.Module):
         '''
         layers = []
         
-        layers.append(nn.Conv2d(in_channels = channels,out_channels = features,kernel_size = ker_size,padding=pad_size, bias = True))
+        layers.append(nn.Conv2d(in_channels = channels,out_channels = features,kernel_size = ker_size,padding=pad_size, bias = False))
         layers.append(nn.ReLU(inplace=True))
         
         for t in range num_layers-2
-            layers.append(nn.Conv2d(in_channels = channels,out_channels = features,kernel_size = ker_size,padding=pad_size, bias = True))
-            layers.append(nn.BatchNorm2d(features))
+            layers.append(nn.Conv2d(in_channels = channels,out_channels = features,kernel_size = ker_size,padding=pad_size, bias = False))
             layers.append(nn.ReLU(inplace=True))
             
-        layers.append(nn.Conv2d(in_channels = features,out_channels = channels,kernel_size = ker_size,padding=pad_size, bias = True))
+        layers.append(nn.Conv2d(in_channels = features,out_channels = channels,kernel_size = ker_size,padding=pad_size, bias = False))
         self.dp_cnn = nn.Sequential(*layers)
     
     def forward(self,x):
-        x = self.dp_cnn(x)
-        return x
+        out = self.dp_cnn(x)
+        return out
     
     '''
     def num_flat_features(self, x):
@@ -53,7 +54,36 @@ class dp_cnn(nn.Module):
         can simply use that value for all layers).
         
     '''
+
+class MyLoss(nn.Module):
+    def __initII(self, size_average=None, reduce=None, reduction: str = 'mean') -> None:
+    super(PatchLoss, self).__init__(size_average, reduce, reduction)
+    
+    def forward(self, output, target, patch_size):
+     # split output and target images into patches
+    #Slice the tensor into patches with patch_size
+    patch_out = output.unfold(0, patch_size, patch_size).unfold(1, patch_size, patch_size)
+    patch_tgt = target.unfold(0, patch_size, patch_size).unfold(1, patch_size, patch_size)
+    
+    #Calculate the abs_loss of each patches
+    num_patch = patch_tgt.nelement()
+    sum_loss = 0
+    
+    #sum-up the absolute loss of all the patches
+    for i in range(list(patch_tgt.size())[1]):
+        sum_loss += f.l1_loss(patch_out[0][i],patch_tgt[0][i])
+    
+    #return the average of the absolute loss of each patches
+    return sum_loss/num_patch
+    
+
 if __name__=="__main__":
+    criterion = MyLoss()
+    dtype = torch.FloatTensor
+    x = Variable(torch.randn(100, 100).type(dtype), requires_grad=False)
+    y = Variable(torch.randn(100, 100).type(dtype), requires_grad=False)
+    loss = criterion(x, y, 10)
+    print(loss)
     net = dp_cnn()
     input = torch.randn(1, 1, 32, 32)
     out = net(input)
--- dp_train.py
+++ dp_train.py
@@ -3,14 +3,15 @@ import argparse
 import numpy as np
 import torch
 import torch.nn as nn
-import torch.optim as optim
 from torch.autograd import Variable
 import uproot
 from dataset import *
-from dp_model import dp_cnn
+from dp_model import dp_cnn, MyLoss
 import torchvision.utils as utils
 from torch.utils.data import DataLoader
 from tensorboardX import SummaryWriter
+import matplotlib.pyplot as plt
+import glob
 
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
@@ -19,7 +20,6 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 parser = argparse.ArgumentParser(description="dp_cnn")
 parser.add_argument("training_path", nargs="?", type=str, default="./data/training", help='path of .root data set to be used for training')
 parser.add_argument("validation_path", nargs="?", type=str, default="./data/validation", help='path of .root data set to be used for validation')
-parser.add_argument("--batchSize", type=int, default=128, help="Training batch size")
 parser.add_argument("--lr", type=float, default=1e-3, help="Initial learning rate")
 parser.add_argument("--epochs", type=int, default=10, help="Number of training epochs")
 parser.add_argument("--num_of_layers", type=int, default=9, help="Number of total layers")
@@ -45,26 +45,23 @@ def main():
     print("We are currently using" + device + "for our training...\n")
     
     # Build model
-    net = dp_cnn()
+    net = dp_cnn(channels=1, num_of_layers=opt.num_of_layers).to(device)
     net.apply(init_weights)
     
     #I'll be using a built-in loss function to guarantee that it is working
-    criterion = nn.L1Loss()
-    
-    # Move to GPUs if available
-    if device == "cuda:0":
-        device_ids = [0]
-        model = nn.DataParallel(net, device_ids=device_ids).cuda()
-        criterion.cuda()
+    criterion = MyLoss()
+    criterion.to(device)
     
     # Optimizer
-    optimizer = optim.Adam(model.parameters(), lr = 0.001)
+    optimizer = optim.Adam(model.parameters(), lr = opt.lr)
     # training
     writer = SummaryWriter(opt.outf)
+    
     step = 0
     for epoch in range(opt.epochs):
         training_files = glob.glob(os.path.join(opt.training_path, '*.root'))
         for training_file in training_files:
+            print("Filename: "+training_file)
             branch = get_all_histograms(training_file)
             length = np.size(branch)
             for i in range(length):
@@ -78,25 +75,20 @@ def main():
                 noisy = torch.from_numpy(noisy)
                 noisy = noisy.unsqueeze(0).unsqueeze(1).to(device)
                 out_train = model(noisy.float()).to(device)
-                loss = criterion(out_train.squeeze(0).squeeze(0), data)
+                loss = criterion(out_train.squeeze(0).squeeze(0), data, 5)
                 loss.backward()
                 optimizer.step()
                 model.eval()
-                if step % 10 ==0:
-                    train_loss = np.append(train_loss, loss.item())
-                step += 1
             model.eval()
         # validation
         validation_files = glob.glob(os.path.join(opt.validation_path,'*root'))
         total_loss = 0
         count = 0
         for validation_file in validation_files:
+            print("Filename: "+validation_file)
             branch = get_all_histograms(training_file)
             length = np.size(branch)
             for i in range(length):
-                model.train()
-                model.zero_grad()
-                optimizer.zero_grad()
                 # prepare data
                 data = get_bin_weights(branch,0).copy()
                 noisy = add_noise(data, args.sigma).copy()
@@ -104,11 +96,15 @@ def main():
                 noisy = torch.from_numpy(noisy)
                 noisy = noisy.unsqueeze(0).unsqueeze(1).to(device)
                 out_train = model(noisy.float()).to(device)
-                total_loss += criterion(out_train.squeeze(0).squeeze(0), data)
+                total_loss += criterion(out_train.squeeze(0).squeeze(0), data, 5).item()
             avg_loss = total_loss / length
             writer.add_scalar('Loss values on validation data', avg_loss, count)
             count += 1
         torch.save(model.state_dict(), os.path.join(opt.outf, 'net.pth'))
-
+        epoch_loss[epoch] = avg_loss
+        print("Epoch #"+str(epoch)+": Average loss: "+str(avg_loss))
+    loss_plot = plt.plot(loss_per_epoch)
+    plt.savefig("loss_plot.png")
+    
 if __name__ == "__main__":
     main()
