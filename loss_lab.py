480b8a167efb3522621b189f79411dcba11845e2
yihengy <yihengy@uchicago.edu>
2020-07-22 16:01:05 +0800

    loss_lab

---
+++ loss_lab.py
@@ -0,0 +1,51 @@
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+from torch.nn import functional as f
+from dataset import *
+from torch.utils.data import DataLoader
+import numpy as np
+from models import DnCNN
+import torch.optim as optim
+
+class PatchLoss(nn.Module):
+    def __initII(self, size_average=None, reduce=None, reduction: str = 'mean') -> None:
+        super(PatchLoss, self).__init__(size_average, reduce, reduction)
+
+    def forward(self, output, target, patch_size):
+        avg_loss = 0
+        print("length:")
+        print(len(output))
+        for i in range(len(output)):
+            output_patches = output[i].unfold(0, patch_size, patch_size).unfold(1, patch_size, patch_size)
+            target_patches = target[i].unfold(0, patch_size, patch_size).unfold(1, patch_size, patch_size)
+            max_patch_loss = 0
+            for i in range(list(output_patches.size())[0]):
+                for j in range(list(output_patches.size())[1]):
+                    print("Patch Tag: i = "+str(i)+", j = "+str(j)+":")
+                    print("Output Patch:")
+                    print(output_patches[i][j])
+                    print("Target Patch:")
+                    print(target_patches[i][j])
+                    tmp = f.l1_loss(output_patches[i][j],target_patches[i][j])
+                    print("L1loss de Patch:"+str(tmp))
+                    print("Max before this patch: " + str(max_patch_loss))
+                    max_patch_loss = max(max_patch_loss, tmp)
+                    print("Max after this patch: " + str(max_patch_loss))
+            avg_loss+=max_patch_loss
+        total =
+        avg_loss/=(list(output_patches.size())[0] * (list(output_patches.size())[1]))
+        return avg_loss;
+
+if __name__ == "__main__":
+    criterion = PatchLoss()
+    dtype = torch.IntTensor
+    # Ordinary Check
+    x = Variable(torch.randn(25, 25).type(dtype), requires_grad=False)
+    print(x)
+    y = Variable(torch.randn(25, 25).type(dtype), requires_grad=False)
+    print(y)
+    loss = criterion(x,y,5)
+    print(loss)
+    print("Finish testing.")
+    
